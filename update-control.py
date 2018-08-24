5536281a90ce6722b6887401a4ed423e8605d31f
Michal Arbet <michal.arbet@ultimum.io>
2018-08-24 17:54:59 +0200

    Something working

--- update-control.py
+++ update-control.py
@@ -19,15 +19,19 @@ for package in reqs:
     deb_package2_found = os.system("grep '^{}\ ' /usr/share/dh-python/dist/cpython2_fallback > /dev/null".format(package))
     if deb_package2_found == 0:
         deb_package2 = subprocess.check_output(
-            "grep '^{}\ ' /usr/share/dh-python/dist/cpython2_fallback | cut -d' ' -f2".format(package), shell=True).decode('utf8').strip()
+            "grep '^{}\ ' /usr/share/dh-python/dist/cpython2_fallback | cut -d' ' -f2 | head -n 1".format(package),
+            shell=True).decode('utf8').strip()
     else:
         deb_package2 = "!!! NOT_FOUND !!!"
 
     deb_package3_found = os.system("grep '^{}\ ' /usr/share/dh-python/dist/cpython3_fallback 2>/dev/null".format(package))
     if deb_package3_found == 0:
         deb_package3 = subprocess.check_output(
-            "grep '^{}\ ' /usr/share/dh-python/dist/cpython3_fallback | cut -d' ' -f2".format(package), shell=True).decode('utf8').strip()
+            "grep '^{}\ ' /usr/share/dh-python/dist/cpython3_fallback | cut -d' ' -f2 | head -n 1".format(package),
+            shell=True).decode('utf8').strip()
     else:
         deb_package3 = "!!! NOT_FOUND !!!"
 
     print("{}|{}|{}".format(package, deb_package2, deb_package3))
+
+    stdout = subprocess.PIPE
